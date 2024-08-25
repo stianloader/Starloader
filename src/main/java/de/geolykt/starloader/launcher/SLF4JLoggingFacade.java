0a6772048464f03f0b431e33bccffb3278c22f6a
Geolykt <admin@geolykt.de>
2024-08-25 16:51:04 +0200

    Ignore micromixin logs if ignoring classloading failures.
    Also updated micromixin to 0.6.4-a20240825, introducing support for
    CanonicalOverwrite.

--- src/main/java/de/geolykt/starloader/launcher/SLF4JLoggingFacade.java
+++ src/main/java/de/geolykt/starloader/launcher/SLF4JLoggingFacade.java
@@ -3,25 +3,35 @@ package de.geolykt.starloader.launcher;
 import org.slf4j.LoggerFactory;
 import org.stianloader.micromixin.transform.api.MixinLoggingFacade;
 
+import net.minestom.server.extras.selfmodification.MinestomRootClassLoader;
+
 class SLF4JLoggingFacade implements MixinLoggingFacade {
 
     @Override
     public void debug(Class<?> clazz, String message, Object... args) {
-        LoggerFactory.getLogger(clazz).trace(message, args);
+        if (MinestomRootClassLoader.getInstance().isThreadLoggingClassloadingFailures()) {
+            LoggerFactory.getLogger(clazz).trace(message, args);
+        }
     }
 
     @Override
     public void error(Class<?> clazz, String message, Object... args) {
-        LoggerFactory.getLogger(clazz).error(message, args);
+        if (MinestomRootClassLoader.getInstance().isThreadLoggingClassloadingFailures()) {
+            LoggerFactory.getLogger(clazz).error(message, args);
+        }
     }
 
     @Override
     public void info(Class<?> clazz, String message, Object... args) {
-        LoggerFactory.getLogger(clazz).info(message, args);
+        if (MinestomRootClassLoader.getInstance().isThreadLoggingClassloadingFailures()) {
+            LoggerFactory.getLogger(clazz).info(message, args);
+        }
     }
 
     @Override
     public void warn(Class<?> clazz, String message, Object... args) {
-        LoggerFactory.getLogger(clazz).warn(message, args);
+        if (MinestomRootClassLoader.getInstance().isThreadLoggingClassloadingFailures()) {
+            LoggerFactory.getLogger(clazz).warn(message, args);
+        }
     }
 }
