dd5b201939f1bdb54c19b7bac7e5d9942f8edb3d
Geolykt <admin@geolykt.de>
2025-09-07 21:37:24 +0200

    Use new RAS module name

--- src/main/java9/module-info.java
+++ src/main/java9/module-info.java
@@ -8,23 +8,24 @@ import de.geolykt.starloader.launcher.service.SLMixinBootstrap;
 import de.geolykt.starloader.launcher.service.SLMixinService;
 
 open module de.geolykt.starloader.launcher {
+    requires transitive java.base;
+
+    requires transitive org.jetbrains.annotations;
     requires transitive org.objectweb.asm.commons;
     requires transitive org.objectweb.asm.util;
     requires transitive org.objectweb.asm;
-    requires transitive org.spongepowered.mixin;
-    requires transitive org.jetbrains.annotations;
     requires transitive org.slf4j;
+    requires transitive org.spongepowered.mixin;
     requires transitive org.stianloader.micromixin.backports;
-    requires transitive java.base;
 
     // requires on filename-based module name
     requires static mixinextras.common;
-    requires static reversible.access.setter;
 
     // Stuff we use but don't expose to everyone
-    requires org.json;
     requires ch.qos.logback.classic;
     requires ch.qos.logback.core;
+    requires de.geolykt.starloader.ras;
+    requires org.json;
     requires org.stianloader.picoresolve;
 
     // Exports
