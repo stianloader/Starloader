5d51abc807c4508c3be054dec65bee5644a47b5a
Geolykt <admin@geolykt.de>
2025-09-07 21:37:24 +0200

    Use new RAS module name

--- src/main/java9/module-info.java
+++ src/main/java9/module-info.java
@@ -1,21 +1,20 @@
-module de.geolykt.starloader.launcher {
+open module de.geolykt.starloader.launcher {
+    requires transitive java.base;
+
     requires transitive org.objectweb.asm.commons;
     requires transitive org.objectweb.asm.util;
     requires transitive org.objectweb.asm;
     requires transitive org.jetbrains.annotations;
     requires transitive org.slf4j;
-    requires transitive java.base;
-
-    // requires on filename-based module name
-    requires static reversible.access.setter;
 
     // Stuff we use but don't expose to everyone
-    requires org.json;
     requires ch.qos.logback.classic;
     requires ch.qos.logback.core;
+    requires de.geolykt.starloader.ras;
+    requires org.json;
+    requires org.stianloader.picoresolve;
     requires org.stianloader.micromixin.transform;
     requires org.stianloader.micromixin.runtime;
-    requires org.stianloader.picoresolve;
 
     // Exports
     exports de.geolykt.starloader;
